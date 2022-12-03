6d8dbc845300fbd89ca9fd620c96d110ce30dc8a
Marc Dwyer <marcdwyer1738@gmail.com>
2022-12-03 14:30:49 -0600

    added navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,17 @@
 export function Navbar() {
   return (
-    <nav className="w-16 h-screen" style={{ border: "1px solid blue" }}>
-      navbar
+    <nav className="w-96 h-screen flex bg-gray-900 text-gray-100">
+      <div className="m-auto flex flex-wrap">
+        <img
+          src="https://i.imgur.com/F4iPL1v.png"
+          className="h-40 w-40 rounded-full mt-5"
+        />
+        <div className="m-auto flex flex-col">
+          <h1 className="m-auto font-bold text-2xl">Marc Dwyer</h1>
+          <h2 className="m-auto font-bold">Front End Enginer</h2>
+          <h2 className="m-auto font-bold">US/EU Citizenship</h2>
+        </div>
+      </div>
     </nav>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "../components/Navbar.tsx";
 export default function Home() {
   return (
     <div id="main-content">
-      <div className="w-screen h-screen">
+      <div className="w-screen h-screen flex font-sans">
         <Navbar />
       </div>
     </div>
