ecb67516c0d6ee6a5203fe6957c418cb081a356c
Marc Dwyer <marcdwyer1738@gmail.com>
2022-12-18 13:09:40 -0600

    work now uses same width as projects

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,8 +21,6 @@ export default function Navbar({ path }: Props) {
   ];
   return (
     <nav className={`w-full flex bg-nav h-20 flex`}>
-      {/* <div className=" m-auto flex flex-wrap"> */}
-
       <div className="flex flex-nowrap mt-auto mb-auto ml-auto flex">
         {links.map((link) => {
           const opacity = link.to === path ? "opacity-100" : "opacity-75";
--- routes/work.tsx
+++ routes/work.tsx
@@ -10,11 +10,13 @@ export default function Work() {
         <title>Marc's Work</title>
       </Head>
       <MainContainer path="/work">
-        <div class="mx-auto w-4/5 flex flex-col">
-          <div class="pt-8 grid grid-cols-1 md:grid-cols-3 items-center gap-4">
-            {WorkExp.map((work) => {
-              return <WorkCard work={work} />;
-            })}
+        <div class="w-full h-full flex overflow-y-auto mb-10">
+          <div class="flex flex-col max-w-screen-lg mx-auto my-16 px(4 sm:6 md:8) space-y-4">
+            <div class="pt-8 grid grid-cols-1 md:grid-cols-3 items-center gap-4">
+              {WorkExp.map((work) => {
+                return <WorkCard work={work} />;
+              })}
+            </div>
           </div>
         </div>
       </MainContainer>
