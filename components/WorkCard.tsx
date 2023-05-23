3f370e04c172b773fdb1236ec02d1bd3493cd566
Marc Dwyer <marcdwyer1738@gmail.com>
2023-05-23 15:58:29 -0500

    fixed work cards

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -5,10 +5,12 @@ type Props = {
 };
 export function WorkCard({ work }: Props) {
   return (
-    <div class="flex flex-col">
-      <h2 class="font-bold">{work.companyName}</h2>
-      <h3>{work.title}</h3>
-      <p>{work.descriptionRole}</p>
+    <div class="flex bg-nav p-5 rounded-lg h-full">
+      <div class="m-auto flex flex-col">
+        <h2 class="font-bold">{work.companyName}</h2>
+        <h3 class="font-bold">{work.title}</h3>
+        <p class="mt-5">{work.descriptionRole}</p>
+      </div>
     </div>
   );
 }
--- routes/work.tsx
+++ routes/work.tsx
@@ -8,7 +8,7 @@ export default function Work() {
       <MainContainer path="/work" title="Work" description="My Work History">
         <div class="w-full h-full flex overflow-y-auto mb-10">
           <div class="flex flex-col max-w-screen-lg mx-auto my-16 px(4 sm:6 md:8) space-y-4">
-            <div class="pt-8 grid grid-cols-1 md:grid-cols-3 items-center gap-4">
+            <div class="grid grid-cols-1 md:grid-cols-3 items-center gap-4 grid-flow-row auto-rows-fr">
               {WorkExp.map((work) => {
                 return <WorkCard work={work} />;
               })}
